355c86e4471c1d2333a03d2be52d6ca8afe61e01
Jeff <jtakaki@murad.com>
2018-11-12 16:08:55 -0800

    mongo set up, route setup

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+	mongoURI: 'mongodb://jeffreytakaki:takaki1@ds159993.mlab.com:59993/developer_connect'
+}
--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+const users = require('./routes/api/users.js')
+const profile = require('./routes/api/profile.js')
+const posts = require('./routes/api/posts.js')
 
 const app = express();
 
+// DB Config
+const db = require('./config/keys.js').mongoURI;
+
+// Connect to MongoDB
+mongoose
+	.connect(db)
+	.then(() => console.log('mongoDB connect'))
+	.catch(err => console.log(err))
+
 
 app.get('/', (req, res) => res.send('hello, you'))
+
+// Use Routes
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
 
 
 const port = process.env.PORT || 5000;
